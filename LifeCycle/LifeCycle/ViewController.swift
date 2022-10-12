a58241cf347bab64a6d80a2fd0dd05094c45be40
ViniciusCleim <vinicius.cleim@gmail.com>
2022-10-11 21:10:43 -0300

    teste para funcoes

--- LifeCycle/LifeCycle/ViewController.swift
+++ LifeCycle/LifeCycle/ViewController.swift
@@ -11,7 +11,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(#function)
         // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        print(#function)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print(#function)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#function)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#function)
     }
 
 
