1f8df6754e33e121df62fcf834c7ba852b5749fe
ViniciusCleim <vinicius.cleim@gmail.com>
2022-10-11 21:02:27 -0300

    first commit

---
+++ LifeCycle/LifeCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LifeCycle
+//
+//  Created by Vinicius Cleim on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
