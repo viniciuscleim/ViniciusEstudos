554f3e7fdf811779742f990b044ec405827d1cd8
ViniciusCleim <vinicius.cleim@gmail.com>
2022-11-07 19:58:24 -0300

    first commit

---
+++ PrimeiroAppCollectionView/PrimeiroAppCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrimeiroAppCollectionView
+//
+//  Created by Vinicius Cleim on 07/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
