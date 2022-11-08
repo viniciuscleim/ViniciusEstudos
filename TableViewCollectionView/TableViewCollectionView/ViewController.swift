33aaf1fe3cbb9aefad1d148c6362e3fa5cfbd593
ViniciusCleim <vinicius.cleim@gmail.com>
2022-11-08 12:44:36 -0300

    continuando o tratamento da collection view dentro da tabelview

---
+++ TableViewCollectionView/TableViewCollectionView/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  TableViewCollectionView
+//
+//  Created by Vinicius Cleim on 07/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configTableView()
+        // Do any additional setup after loading the view.
+    }
+    
+    func configTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(CarTableViewCell.nib(), forCellReuseIdentifier: CarTableViewCell.identifier)
+        
+    }
+    
+ 
+}
+
+extension ViewController: UITableViewDelegate {
+    
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CarTableViewCell.identifier, for: indexPath) as? CarTableViewCell
+        return cell ?? UITableViewCell()
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 182
+    }
+    
+    
+}
+
+extension ViewController: UICollectionViewDelegate {
+    
+    
+}
+    
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+}
+
